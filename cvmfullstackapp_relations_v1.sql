8c93ebc676723f97b52ebcc8e1b5adf3a6a6636e
Jaxon McClelland <jmcclelland1844@protonmail.com>
2024-03-27 00:12:04 -0700

    Updated schema for milestone2

--- cvmfullstackapp_relations_v1.sql
+++ cvmfullstackapp_relations_v1.sql
@@ -1,24 +1,21 @@
-CREATE TABLE User (
-    UserID VARCHAR(25) PRIMARY KEY,
-    UserName VARCHAR,
-    AverageStars DECIMAL,  
-    YelpingSince DATETIME,
-);
-
 CREATE TABLE Review(
+    ReviewID INT PRIMARY KEY,
     UserID VARCHAR(25) FOREIGN KEY REFERENCES [User].[UserID],
     BusinessID VARCHAR(25) FOREIGN KEY REFERENCES [Business].[BusinessID],    Review VARCHAR(1024) NOT NULL, 
     Funny INTEGER DEFAULT 0,
     Cool INTEGER DEFAULT 0,
     Useful INTEGER DEFAULT 0,
-    PRIMARY KEY (UserID, BusinessID),
+    ReviewStars INTEGER DEFAULT 0,
+    ReviewText VARCHAR(1024),
 );
 
 
 CREATE TABLE CheckIn(
     UserID VARCHAR(25) FOREIGN KEY REFERENCES [User].[userID],
     BusinessID VARCHAR(25) FOREIGN KEY REFERENCES [Business].[BusinessID],
-    CheckInDate DATETIME NOT NULL,
+    DAY VARCHAR(10) NOT NULL,
+    TIME VARCHAR(10) NOT NULL,
+    AMOUNT INTEGER NOT NULL,
     PRIMARY KEY (UserID, BusinessID),
 );
 
@@ -28,13 +25,28 @@ CREATE TABLE Business(
     Stars INTEGER,
     IsOpen BOOLEAN,
     PriceRange INTEGER NULL,
+    ReviewRating INTEGER,
+    CheckIns INTEGER,
+    ReviewCount INTEGER,
     AddressID INTEGER FOREIGN KEY REFERENCES [Address].[AddressID]
 );
 
 CREATE TABLE BusinessCategory(
     BusinessID VARCHAR(25) FOREIGN KEY REFERENCES [Business].[BusinessID],
     CategoryID INT FOREIGN KEY REFERENCES [Category].[CategoryID],
-)
+    PRIMARY KEY(BusinessID, CategoryID)
+);
+
+CREATE TABLE BusinessAttribute(
+    BusinessID VARCHAR(25) FOREIGN KEY REFERENCES [Business].[BusinessID],
+    AttributeID INT FOREIGN KEY REFERENCES [Attribute].[AttributeID],
+);
+
+CREATE TABLE Attribute(
+    AttributeID INT PRIMARY KEY,
+    AttributeName VARCHAR(20) NOT NULL,
+    AttributeValue BOOLEAN NOT NULL
+);
 
 CREATE TABLE Category (
     CategoryID INT PRIMARY KEY,
